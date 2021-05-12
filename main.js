8ebea58859ff00f6e1beb5b44aee827df860cc32
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-12 10:03:31 +0200

    add new styles

--- main.js
+++ main.js
@@ -1,47 +1,43 @@
-'use strict'
+"use strict";
 //const
 const button = document.querySelector(".js-button");
-const inputNumber = document.querySelector (".js-input");
-const clue = document.querySelector (".js-clue");
-const tryNumber = document.querySelector (".js-try");
+const inputNumber = document.querySelector(".js-input");
+const clue = document.querySelector(".js-clue");
+const tryNumber = document.querySelector(".js-try");
 
 // generate random number
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
+  return Math.ceil(Math.random() * max);
+}
 const randomNumber = getRandomNumber(100);
-console.log (randomNumber);
+console.log(randomNumber);
 
 //counter
 let counter = 0;
-function handlerCounter (){
-    tryNumber.innerHTML= `numero de intentos: `+ (counter +=1);
+function handlerCounter() {
+  tryNumber.innerHTML = `numero de intentos: ` + (counter += 1);
 }
 
 //try
-function writeNumber(){
-    const number = parseInt(inputNumber.value);
-    if(number <= 0 || number >= 100){
-        clue.innerHTML= 'El número deber de estar ente 0 y 100🙄' ;
-    }
-    else if (number === randomNumber) {
-        clue.innerHTML = '¡¡Has ganado campeona!! 🤟🏻';
-    }
-    else if (number < randomNumber){
-        clue.innerHTML = 'Demasiado bajo 🧐';
-    }
-    else if (number > randomNumber) {
-        clue.innerHTML = 'Demasiado alto 🧐';
-    }
+function writeNumber() {
+  const number = parseInt(inputNumber.value);
+  if (number <= 0 || number >= 100) {
+    clue.innerHTML = "El número deber de estar ente 0 y 100🙄";
+  } else if (number === randomNumber) {
+    clue.innerHTML = "¡¡Has ganado campeona!! 🤟🏻";
+  } else if (number < randomNumber) {
+    clue.innerHTML = "Demasiado bajo 🧐";
+  } else if (number > randomNumber) {
+    clue.innerHTML = "Demasiado alto 🧐";
+  }
 }
 
 //funtion que agrupe a todas
-function handlerClick(event){
-    event.preventDefault();
-    writeNumber();
-    handlerCounter();
+function handlerClick(event) {
+  event.preventDefault();
+  writeNumber();
+  handlerCounter();
 }
 
 //eventListener
-button.addEventListener('click', handlerClick);
-
+button.addEventListener("click", handlerClick);
