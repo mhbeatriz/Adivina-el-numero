dc7875d34c64965621c9ba226fdf5ad7479485b5
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-12 09:23:42 +0200

    add prevendefault

--- main.js
+++ main.js
@@ -3,31 +3,35 @@
 const button = document.querySelector(".js-button");
 const inputNumber = document.querySelector (".js-input");
 const clue = document.querySelector (".js-clue");
-const tryNumber = docuement.querySelector (".js-try");
+const tryNumber = document.querySelector (".js-try");
 
 // generate random number
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
-const randomNumber = getRandomNumber();
+const randomNumber = getRandomNumber(100);
+console.log (randomNumber);
 
 //try
-
 function writeNumber(){
-    let number = parseInt(inputNumber.value);
+    const number = parseInt(inputNumber.value);
     if(number <= 0 || number >= 100){
-        clue.innerHTML= 'Has ganado campeona';
-    }else if (number === randomNumber) {
-        clue.innerHTML = 'El número deber de estar ente 0 y 100';
-    }else if (number < randomNumber){
+        clue.innerHTML= 'El número deber de estar ente 0 y 100';
+    }
+    else if (number === randomNumber) {
+        clue.innerHTML = 'Has ganado campeona';
+    }
+    else if (number < randomNumber){
         clue.innerHTML = 'Demasiado bajo';
-    }else {
+    }
+    else if (number > randomNumber) {
         clue.innerHTML = 'Demasiado alto';
     }
 }
 
 //funtion que agrupe a todas
-function handlerClick(){
+function handlerClick(event){
+    event.preventDefault();
 writeNumber();
 }
 
